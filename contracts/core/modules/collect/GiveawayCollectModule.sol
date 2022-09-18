efe72119aa3a9dea2b149efb2f23120f6c3e21be
starwalker00 <aaronwalkerup@gmail.com>
2022-09-18 14:31:06 +0200

    feat: setup giveaway collect module VRF

--- contracts/core/modules/collect/GiveawayCollectModule.sol
+++ contracts/core/modules/collect/GiveawayCollectModule.sol
@@ -4,9 +4,12 @@ pragma solidity 0.8.10;
 
 import {ICollectModule} from '../../../interfaces/ICollectModule.sol';
 import {Errors} from '../../../libraries/Errors.sol';
+import {DataTypes} from '../../../libraries/DataTypes.sol'; // Publication Struct
 import {ModuleBase} from '../ModuleBase.sol';
 import {IERC721Enumerable} from '@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol';
 import {ILensHub} from '../../../interfaces/ILensHub.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
 
 /**
  * @title GiveawayCollectModule
@@ -17,7 +20,16 @@ import {ILensHub} from '../../../interfaces/ILensHub.sol';
  * This module works by triggering a giveaway of an amount provisionned by the publisher to a
  * random collector when collector threshold is reached.
  */
-contract GiveawayCollectModule is ModuleBase, ICollectModule {
+contract GiveawayCollectModule is ModuleBase, ICollectModule, VRFConsumerBaseV2 {
+    // VRF temporary config
+    VRFCoordinatorV2Interface COORDINATOR;
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 2;
+    uint64 s_subscriptionId = 1;
+
     enum GiveawayStatus {
         Active,
         Triggered,
@@ -27,11 +39,20 @@ contract GiveawayCollectModule is ModuleBase, ICollectModule {
         uint256 currencyAmount;
         uint256 collectAmount;
         GiveawayStatus status;
+        uint256 s_requestId;
+        uint256 s_randomWord;
     }
+
+    // Maps giveaway data to a publication
     mapping(uint256 => mapping(uint256 => GiveawayPublicationData))
         internal _dataByPublicationByProfile;
 
-    constructor(address hub) ModuleBase(hub) {}
+    // Maps VRF request to a publication
+    mapping(uint256 => DataTypes.PublicationStruct) internal _pubByRequestId;
+
+    constructor(address hub) ModuleBase(hub) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    }
 
     /**
      * @notice This collect module triggers a giveaway at a provided collected amount. Thus, we need to decode data.
@@ -74,9 +95,29 @@ contract GiveawayCollectModule is ModuleBase, ICollectModule {
         address collectNFT = ILensHub(HUB).getCollectNFT(profileId, pubId);
         uint256 totalSupply = IERC721Enumerable(collectNFT).totalSupply();
         if (_dataByPublicationByProfile[profileId][pubId].collectAmount >= totalSupply) {
-            _triggerGiveaway();
+            // _triggerGiveaway();
+            _requestRandomWords(profileId, pubId);
         }
     }
 
     function _triggerGiveaway() internal {}
+
+    function _requestRandomWords(uint256 profileId, uint256 pubId) internal {
+        uint256 requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        _dataByPublicationByProfile[profileId][pubId].s_requestId = requestId; // not strictly necessary
+        _pubByRequestId[requestId].profileIdPointed = profileId;
+        _pubByRequestId[requestId].pubIdPointed = pubId;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256 profileId = _pubByRequestId[requestId].profileIdPointed;
+        uint256 pubId = _pubByRequestId[requestId].pubIdPointed;
+        _dataByPublicationByProfile[profileId][pubId].status = GiveawayStatus.Done;
+    }
 }
